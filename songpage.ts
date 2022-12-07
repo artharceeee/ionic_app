afbe3a43f59fd30e94def54f46dea015839d539c
artharceeee <119767175+artharceeee@users.noreply.github.com>
2022-12-08 07:41:47 +0800

    Create songpage.ts

---
+++ songpage.ts
@@ -0,0 +1,31 @@
+songs = [ {
+
+title: "Believer", subtitle: "Imagine Dragons", img: "/assets/believer.jpg", path:
+
+"/assets/songs/believer.mp3"
+
+},
+
+{
+
+title: "Fifa 2010", subtitle: "FIFA 2010 Theme
+
+img: "/assets/fifa.png",
+
+path:
+
+"/assets/songs/fifa_2010.mp3"
+
+Song",
+
+},
+
+{
+
+title: "Thunder", subtitle: "Imagine Dragons", img: "/assets/thunder.jpg", path:
+
+"/assets/songs/thunder.mp3"
+
+];
+
+}
